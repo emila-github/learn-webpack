dd3bb817669091d23ad6f2ceefed8eb1fc65504b
王常键 <changjianwang@cyou-inc.com>
2016-07-28 16:28:48 +0800

    demo练习

---
+++ content.js
@@ -0,0 +1,1 @@
+module.exports = "It works from content.js.111122233";
---
+++ entry.js
@@ -0,0 +1,4 @@
+//document.write("看看如何让它工作！");
+//require("!style!css!./style.css");
+require('./style.css');
+document.write(require("./content.js"));
